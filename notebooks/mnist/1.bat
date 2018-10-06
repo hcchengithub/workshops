3fc086a503452daf6b252f59565be409eee8742a
H.C. Chen <hcchen5600@gmail.com>
2018-10-06 14:59:42 +0800

    study

---
+++ notebooks/mnist/1.bat
@@ -0,0 +1,5 @@
+mklink t10k-images-idx3-ubyte.gz c:\Users\hcche\Downloads\mnist\t10k-images-idx3-ubyte.gz
+mklink t10k-images-idx3-ubyte.gz  c:\Users\hcche\Downloads\mnist\t10k-images-idx3-ubyte.gz  
+mklink t10k-labels-idx1-ubyte.gz  c:\Users\hcche\Downloads\mnist\t10k-labels-idx1-ubyte.gz
+mklink train-images-idx3-ubyte.gz c:\Users\hcche\Downloads\mnist\train-images-idx3-ubyte.gz
+mklink train-labels-idx1-ubyte.gz c:\Users\hcche\Downloads\mnist\train-labels-idx1-ubyte.gz
